b2cb15a783382527eb2e7761e867a41597a0a949
21mb358@esisalama.org <21mb358@esisalama.org>
2024-05-27 09:41:02 +0200

    ajout du module os

--- projets/devine_mot.py
+++ projets/devine_mot.py
@@ -1,14 +1,17 @@
 import random
+import os
 
-nombre_mystere = random.randint(1,10)
+
 
 def devine():
+    nombre_mystere = random.randint(1,10)
     essaie = 0
     while essaie <=3:
 
         nombre_user = int(input("entrer votre nombre: "))
         if nombre_mystere == nombre_user:
             print("vous avez reussi a trouver le bon nombre ")
+            demande()
             break
         else:
             
@@ -19,7 +22,7 @@ def devine():
                 print("le nombre est plus petit")
                 print(f"il vous reste {3 -essaie}")
             essaie += 1
-        if essaie== 0:
+        if essaie== 4:
             demande()
         
 def demande():
