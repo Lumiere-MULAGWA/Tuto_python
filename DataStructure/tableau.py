c3db994aa6cd34a7e2e4dd5f2342acf2c4d110f3
21mb358@esisalama.org <21mb358@esisalama.org>
2024-05-27 10:12:50 +0200

    ajout de clear

--- DataStructure/tableau.py
+++ DataStructure/tableau.py
@@ -7,14 +7,20 @@ class Array:
         self.types = _types
         self.array = []
     
-    def add(self,*element):
+    def add(self,element):
         self.array.append(element)
 
     def remove(self,index):
         del self.array[index]
+    def clear(self):
+        self.array.clear()
         
 table = Array(5,int)
-table.add(4,5,6,7)
+table.add(4)
+table.add(5)
+table.add(6)
+table.add(3)
 table.remove(2)
+
 print(table.array)
 
