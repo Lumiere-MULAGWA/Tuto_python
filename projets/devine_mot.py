696386f4c17dbc588a9faca64b78e310a720cac6
21mb358@esisalama.org <21mb358@esisalama.org>
2024-05-27 09:46:34 +0200

    modification de l'affichage

--- projets/devine_mot.py
+++ projets/devine_mot.py
@@ -6,30 +6,34 @@ import os
 def devine():
     nombre_mystere = random.randint(1,10)
     essaie = 0
+    os.system("clear")
+    print("+"*50)
+    print("+\t Bienvenue dans le jeu devine mot  \t+")
+    print("+"*50)
     while essaie <=3:
 
-        nombre_user = int(input("entrer votre nombre: "))
+        nombre_user = int(input("\tentrer votre nombre: "))
         if nombre_mystere == nombre_user:
-            print("vous avez reussi a trouver le bon nombre ")
+            print("\tvous avez reussi a trouver le bon nombre ")
             demande()
             break
         else:
             
             if nombre_user < nombre_mystere:
-                print("le nombre est plus grand ")
-                print(f"il vous reste {3 -essaie}")
+                print("\tle nombre est plus grand ")
+                print(f"\til vous reste {3 -essaie}")
             else :
-                print("le nombre est plus petit")
-                print(f"il vous reste {3 -essaie}")
+                print("\tle nombre est plus petit")
+                print(f"\til vous reste {3 -essaie}")
             essaie += 1
         if essaie== 4:
             demande()
         
 def demande():
-    print("voulez vous recommencer ? (o/n) ")
-    choix = input(">>> ")
+    print("\tvoulez vous recommencer ? (o/n) ")
+    choix = input("\t\t>>> ")
     if choix != "o":
-        print("Bye bye ")
+        print("\t\tBye bye (^_^)")
     else:
         devine()
 devine()
