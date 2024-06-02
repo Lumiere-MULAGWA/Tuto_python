e0f26299c11d11d30ec3cf81500db30a3c3b1359
lmr lumiere <126115024+Lumiere-MULAGWA@users.noreply.github.com>
2024-06-02 14:36:42 +0200

    Create boucle.py

---
+++ basic/begin/boucle.py
@@ -0,0 +1,9 @@
+"""
+les boucle en python sont une structure iterative 
+il y a la boucle for et la boucle while
+"""
+for i in range(10):
+  print(f"{i} est la {i+1} ieme iteration")
+age = 0 
+while (age < = 18):
+  print(f"age vaut :{age}")
