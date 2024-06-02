6cd6766f9c585a86676b77425b0a9330c82c21f3
lmr lumiere <126115024+Lumiere-MULAGWA@users.noreply.github.com>
2024-06-02 14:48:56 +0200

    mise a jour de place

--- DataStructure/text.py
+++ DataStructure/text.py
@@ -1,7 +1,8 @@
 text ="bonjour le monde "
-index=''
-for i in range(len(text)):
-    if text[i] == "o":
-        index = index +","+ str(i)
-        
-print(index)
+def place(text,caractere):
+    index=''
+    for i in range(len(text)):
+        if text[i] == caractere:
+            index = index +","+ str(i)
+            print(index)
+place(text,"o")
