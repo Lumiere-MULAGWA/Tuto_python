e74daeaa6f14e5cd4e5a624ebba86055c971e3c7
lmr lumiere <126115024+Lumiere-MULAGWA@users.noreply.github.com>
2024-06-02 14:42:47 +0200

    boucle for

--- basic/begin/boucle.py
+++ basic/begin/boucle.py
@@ -2,10 +2,15 @@
 les boucle en python sont une structure iterative 
 il y a la boucle for et la boucle while
 la boucle for marche de deux maniere
-  1. sur les nombre avec le mot cle range (range(nonbre) genere un nombre de 0 jusqu'a ce nombre
+  1. sur les nombres avec le mot cle range (range(nonbre) genere un nombre de 0 jusqu'a ce nombre
+  2. sur les chaines de caracter on garde juste le in et on va parcourir la chaine de caractere une a une 
 """
 for i in range(10):
   print(f"{i} est la {i+1} ieme iteration")
 age = 0 
 while (age < = 18):
   print(f"age vaut :{age}")
+
+#for sur les nombres
+for i in range(10):
+  print(f"{i} est la {i+1} ieme iteration")
