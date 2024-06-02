46c0dd88a749c58939babea9ffab822bcd1edff7
lmr lumiere <126115024+Lumiere-MULAGWA@users.noreply.github.com>
2024-06-02 14:40:25 +0200

    Update boucle.py

--- basic/begin/boucle.py
+++ basic/begin/boucle.py
@@ -1,6 +1,8 @@
 """
 les boucle en python sont une structure iterative 
 il y a la boucle for et la boucle while
+la boucle for marche de deux maniere
+  1. sur les nombre avec le mot cle range (range(nonbre) genere un nombre de 0 jusqu'a ce nombre
 """
 for i in range(10):
   print(f"{i} est la {i+1} ieme iteration")
